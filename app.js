64f96366efefd434da0e4d2fcffffeab982a24e6
TaniaUdod <taniaudod28@gmail.com>
2024-02-04 13:40:40 +0200

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/users", usersRouter);
+app.use("/users", usersRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((_, res) => {
